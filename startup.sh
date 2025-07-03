24a01f97346860ada28fbc34a6330331378e7334
Jayanti Lal <jayanti.lal@tvsmotor.com>
2025-07-03 12:54:23 +0530

    startup change req3

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
-python app.py
-#gunicorn --bind 0.0.0.0:${WEBSITES_PORT:-8000} app:app
+#python app.py
+gunicorn --bind 0.0.0.0:${WEBSITES_PORT:-8000} app:app
