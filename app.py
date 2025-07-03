029ec983c818312e8cd690778db526bdf769d525
Jayanti Lal <jayanti.lal@tvsmotor.com>
2025-07-03 11:35:52 +0530

    Gunicorn issue

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def generate_personalized_pitches():
     return jsonify(pitch_dict)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
